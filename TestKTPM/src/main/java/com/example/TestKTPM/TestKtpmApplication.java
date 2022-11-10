df4a9dc2427ba1fc327f8b90d3e81122b4d4e2cc
ThaiAn31 <nguyenthaian31102001@gmail.com>
2022-11-10 13:14:28 +0700

    first commit

---
+++ TestKTPM/src/main/java/com/example/TestKTPM/TestKtpmApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestKTPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKtpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestKtpmApplication.class, args);
+	}
+
+}
---
+++ TestKTPM/src/test/java/com/example/TestKTPM/TestKtpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestKTPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKtpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
